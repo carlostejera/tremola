2298706260031a3e5d26d84bbe46b60ccf2ecf7c
LarsZauberer <wasser.ian@gmail.com>
2021-09-08 09:45:02 +0200

    Refactored image compression and conversion

--- app/src/main/java/nz/scuttlebutt/tremola/MainActivity.kt
+++ app/src/main/java/nz/scuttlebutt/tremola/MainActivity.kt
@@ -174,38 +174,44 @@ class MainActivity : Activity() {
         // Retrieve Image from taking the image
         if (requestCode == 1 && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
-            // scale to 96xN pixels
-            val resized = Bitmap.createScaledBitmap(imageBitmap, 96,
-                96*imageBitmap.height/imageBitmap.width, true)
-            // Convert imageBitmap to ByteArray
-            val stream = ByteArrayOutputStream()
-            resized.compress(Bitmap.CompressFormat.JPEG, 85, stream)
-            // Send Byte Array to javascript, so it can be displayed on the image element with the id showImg
-            // ByteArray encode base64
-            var img: String = Base64.encodeToString(stream.toByteArray(), Base64.NO_WRAP)
-            Log.d("image", "${img.length}B <${img}>")
-            // tremolaState.wai.eval("b2f_new_image('${img}')")
+            
+            val img = compressAndEncodeBitmap(imageBitmap)
+
             tremolaState.wai.eval("sendImg('${img}')")
         }
 
         if (requestCode == 1111 && resultCode == RESULT_OK) {
             val imageBitmap = data?.data //as Bitmap
             val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageBitmap)
-
-            val resized = Bitmap.createScaledBitmap(bitmap, 96,
-                96*bitmap.height/bitmap.width, true)
-            //convert imageBitmap to Byte Array
-
-            val stream = ByteArrayOutputStream()
-            resized.compress(Bitmap.CompressFormat.JPEG, 85, stream)
-
-            var img: String = Base64.encodeToString(stream.toByteArray(), Base64.NO_WRAP)
-            Log.d("image", "${img.length}B <${img}>")
+            
+            val img = compressAndEncodeBitmap(bitmap)
 
             tremolaState.wai.eval("showImagePreview('${img}')")
         }
 
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun compressAndEncodeBitmap(bitmap: Bitmap): String {
+        /*
+            Compresses an bitmap and converts it to an base64 string.
+            returns the string
+         */
+        // scale to 96xN pixels
+        val resized = Bitmap.createScaledBitmap(bitmap, 96,
+            96*bitmap.height/bitmap.width, true)
+
+        // Convert imageBitmap to ByteArray
+        val stream = ByteArrayOutputStream()
+        resized.compress(Bitmap.CompressFormat.JPEG, 85, stream)
+
+        // ByteArray encode base64
+        var img: String = Base64.encodeToString(stream.toByteArray(), Base64.NO_WRAP)
+
+        // Log Message
+        Log.d("image", "${img.length}B <${img}>")
+
+        return img
     }
 
     override fun onResume() {
